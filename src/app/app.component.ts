dc74f110e41eb3a7a0a12eed6905a7c1158b1dd3
nikki boyd and scott fraser <nikki+scott@>
2018-08-14 09:21:08 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from  './models/recipe.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Recipe Box';
+  recipes: Recipe[] = [
+    new Recipe(
+      'Doughnut',
+      ['Dough', 'Nuts', 'More Nuts'],
+      ['Make Dough', 'Fry that shit', 'Eat it']),
+    new Recipe(
+      'Pizza',
+      ['Dough', 'Sauce', 'Cheese'],
+      ['Make Dough', 'Put Stuff On It', 'Eat it']),
+    new Recipe(
+      'Salad',
+      ['Lettuce', 'Stuff', 'Dressing'],
+      ['Toss that shit', 'Eat it']),
+
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { IngredientsComponent } from './ingredients/ingredients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngredientsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
